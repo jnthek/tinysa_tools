28e6af5a4d9b396758fe184161512c89b799c09a
VU3VWB <vu3vwb@gmail.com>
2023-08-05 07:16:43 +0800

    Modify the run script

--- vis_tools/sample_run.sh
+++ vis_tools/sample_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # python3 Tinysa_animate_waterfall_scanraw.py -s /dev/cu.usbmodem4001 -f 87e6:108e6:100e3 -N 200 -T 101
-python3 Tinysa_animate_waterfall_scanraw.py -s /dev/ttyACM0 -f 10e6:30e6:100e3 -N 200 -T 101
+python3 Tinysa_animate_waterfall_scanraw.py -s /dev/ttyACM0 -f 146e6:148e6:10e3 -N 1024 -T 101
